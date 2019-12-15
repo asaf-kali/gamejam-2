e92b53c795df6aef7000e7e34a5aff25d72bfd8c
Yael Renous <yael.renous@mail.huji.ac.il>
2019-12-15 22:33:21 +0200

    first commit

---
+++ Sisyphus/Assets/Script/LineController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController : MonoBehaviour
+{
+
+  public Vector2 moveDirection;
+  public float moveSpeed;
+  public Rigidbody2D rigidbody;
+
+  private readonly float endY=-10f;
+  private readonly Vector3 startPoint = new Vector3(-19.4f, 10.1f, 0);
+
+    // Start is called before the first frame update
+
+  void Start()
+  {
+    //var v = Camera.main.camera.orthographicSize;
+
+    Debug.Log(tag + " y pos = " + transform.localPosition.y.ToString() + "x pos = "+ transform.position.x.ToString());
+
+  }
+    // Update is called once per frame
+ void Update()
+  {
+    if (transform.localPosition.y <= endY)
+      transform.localPosition = startPoint;
+    else
+      transform.Translate(moveDirection * Time.deltaTime*moveSpeed);
+    
+
+  }
+
+  private void FixedUpdate()
+  {
+    //rigidbody.MovePosition(rigidbody.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
+  }
+
+}
